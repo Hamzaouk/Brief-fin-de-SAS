377f03715d613234e66ec481a3c1387d33e4f0e3
OUKHATOU HAMZA <hamzaoukhatou@gmail.com>
2024-10-30 17:11:33 +0100

    creer les structures et declaration des fonctions

--- sas.c
+++ sas.c
@@ -3,3 +3,20 @@
 #include <string.h>
 
 #define MAX_TACHES 100
+
+struct Tache {
+    char titre[50];
+    char desc[100];
+    char date[11];
+    char prio[5];
+    char statut[10];
+};
+
+struct Tache taches[MAX_TACHES];
+int n_tch = 0;
+
+
+void ajout_tch();
+void affiche_tch();
+void modif_tch();
+void suppr_tch();
